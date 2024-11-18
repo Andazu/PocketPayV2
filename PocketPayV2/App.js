d300e53ecc65bfa439ae5b60db83863235e824cc
Andazu <andersmazen@gmail.com>
2024-11-18 13:29:49 +0100

    Expo and NativeWind setup!

---
+++ PocketPayV2/App.js
@@ -0,0 +1,15 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import "./global.css";
+
+export default function App() {
+  return (
+    <View className='flex-1 justify-center items-center bg-white'>
+      <StatusBar style='auto' />
+      <Text className='text-center mt-3 text-2xl font-light text-orange-300'>
+        Login
+      </Text>
+      {/* Additional components goes here */}
+    </View>
+  );
+}
